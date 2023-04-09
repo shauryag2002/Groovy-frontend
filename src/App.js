0ab7221f410a3afaf49b6579a2632d2c1092137d
Shaurya Gupta <shauryag1000@gmail.com>
2023-04-09 15:46:49 +0530

    added UI and choose file functionality

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import "./component/allstyle.css";
 import Navbar from "./component/Navbar";
 import Post from "./component/Post";
+// import "./component/register_update.css";
 import { Routes, Route } from "react-router-dom";
 import Register from "./component/Register";
 import axios from "axios";
@@ -16,6 +17,8 @@ import Profile from "./component/Profile";
 import Edit from "./component/Edit";
 import Popup from "./component/Popup";
 import PostOne from "./component/PostOne";
+import Login_update from "./component/Login_update";
+import Register_update from "./component/Register_update";
 
 function App() {
   const [user, setUser] = useState("");
@@ -47,8 +50,11 @@ function App() {
     <>
       {/* <Navbar /> */}
       <Routes>
-        <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        {/* <Route path="/register" element={<Register />} /> */}
+        <Route path="/register" element={<Register_update />} />
+
+        {/* <Route path="/login" element={<Login />} /> */}
+        <Route path="/login" element={<Login_update />} />
         <Route path="/post" element={<Post />} />
         <Route path="/post/:id" element={<PostOne />} />
 
--- src/component/Createpost.jsx
+++ src/component/Createpost.jsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import { useNavigate } from "react-router";
 const Createpost = () => {
   const navigate = useNavigate();
+  const [image, setImage] = useState(null);
   useEffect(() => {
     if (!localStorage.getItem("email") || !localStorage.getItem("password")) {
       // history.push("/login");
@@ -18,6 +19,7 @@ const Createpost = () => {
     // localStorage.getItem("email");
     // localStorage.getItem("password");
     // if()
+
     const userOne = await axios
       .post("https://groovybackend-shauryag2002.vercel.app/auth/login", {
         email: localStorage.getItem("email"),
@@ -30,23 +32,54 @@ const Createpost = () => {
     // return userOne
   };
   const createPost = async () => {
-    const img = document.getElementById("img");
-    const desc = document.getElementById("desc");
-    const create = await axios.post(
-      "https://groovybackend-shauryag2002.vercel.app/post/",
-      {
-        userId: user._id,
-        img: img.value,
-        desc: desc.value,
-      }
-    );
+    const formData = new FormData();
+    formData.append("file", image);
+    formData.append("upload_preset", "zj2wibx6");
+    await axios
+      .post("https://api.cloudinary.com/v1_1/dxh9yv3mp/image/upload", formData)
+      .then(async (res) => {
+        console.log(res.data.url);
+        const img = document.getElementById("img");
+        const desc = document.getElementById("desc");
+        const create = await axios.post(
+          "https://groovybackend-shauryag2002.vercel.app/post/",
+          {
+            userId: user._id,
+            img: res.data.url,
+            desc: desc.value,
+          }
+        );
+      });
+    navigate("/");
+  };
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0];
+    setImage(selectedImage);
   };
   return (
     <div>
       <Navbar />
       {/* Createpost */}
-      <input type="text" id="img" placeholder="Image" />
-      <input type="text" id="desc" placeholder="Description" />
+      <h1 style={{ marginBottom: "20px" }}>Create Post</h1>
+      <span>Image:</span>
+      <input
+        style={{ marginBottom: "20px" }}
+        type="file"
+        id="img"
+        accept="image/*"
+        placeholder="Image"
+        onChange={handleImageChange}
+        className="input inputt"
+      />
+      <br />
+      <span>Description:</span>
+      <input
+        type="text"
+        id="desc"
+        placeholder="Description"
+        className="input inputt"
+      />
+      <br />
       <button onClick={createPost}>Create Post</button>
     </div>
   );
